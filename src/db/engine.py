42174340c8dbb574322e35bfe6d78105f16f400d
Leontiy Tsagoiko <leon@MacBook-Air-Leontiy.local>
2023-06-01 02:39:35 +0300

    Initial commit

---
+++ src/db/engine.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, \
+    create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def build_async_engine(url: Union[URL, str]) -> AsyncEngine:
+    return create_async_engine(
+        url=url, echo=True, pool_pre_ping=True, future=True
+    )
+
+
+def get_session_maker(engine: AsyncEngine) -> sessionmaker:
+    return sessionmaker(engine=engine, class_=AsyncSession)
+
+# async def proceed_schemas(engine: AsyncEngine, metadata) -> None:
+#     async with engine.begin() as conn:
+#         await conn.run_sync(metadata.create_all)
