783c2f508b5d70595232264a55023a3103ba3c25
Leontiy Tsagoiko <leon@MacBook-Air-Leontiy.local>
2023-06-01 03:06:54 +0300

    Add: bot launch logic in __main__

---
+++ src/bot/__main__.py
@@ -0,0 +1,29 @@
+from aiogram import Dispatcher, Bot
+from src.configuration import conf
+import asyncio
+import logging
+from logic import register_user_commands
+from src.db.engine import build_async_engine, get_session_maker
+
+
+async def main() -> None:
+    logging.basicConfig(level=logging.DEBUG)
+
+    dp = Dispatcher()
+    bot = Bot(token=conf.bot.token)  # parse_mode="HTML"
+
+    register_user_commands(dp)
+
+    postgres_url = conf.db.build_connection_url()
+    async_engine = build_async_engine(url=postgres_url)
+    session_maker = get_session_maker(engine=async_engine)
+
+    # await proceed_schemas(async_engine, Base.metadata)
+    await dp.start_polling(bot, session_maker=session_maker)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Bot stopped")
